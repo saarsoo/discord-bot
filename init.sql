a628abf0b1b1978006b7ebdb7ab0a3c3e23cc5fd
Johan Saarsoo <johan@saarsoo.se>
2024-08-05 19:00:20 +0200

    Add recipes table

--- init.sql
+++ init.sql
@@ -1,11 +1,18 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   user_id BIGINT PRIMARY KEY,
   user_name VARCHAR NOT NULL
 );
 
-CREATE TABLE professions (
-    user_id BIGINT,
-    profession_name VARCHAR NOT NULL,
+CREATE TABLE IF NOT EXISTS professions (
+  user_id BIGINT,
+  profession_name VARCHAR NOT NULL,
 
-    PRIMARY KEY (user_id, profession_name)
+  PRIMARY KEY (user_id, profession_name)
+);
+
+CREATE TABLE IF NOT EXISTS recipes (
+  user_id BIGINT,
+  recipe_name VARCHAR NOT NULL,
+
+  PRIMARY KEY (user_id, recipe_name)
 );
