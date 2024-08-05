7f59d109ed58e135c2a137c8c78044365874f39b
Johan Saarsoo <johan@saarsoo.se>
2024-08-05 19:05:42 +0200

    Add recipe commands

--- src/discord_bot/app.py
+++ src/discord_bot/app.py
@@ -8,11 +8,16 @@ from discord.ext import commands
 from discord_bot.database import (
     add_profession,
     add_user,
+    add_user_recipe,
     get_all_professions,
+    get_all_recipes,
     get_user_professions,
+    get_user_recipes,
     remove_profession,
+    remove_user_recipe,
     validate_connection,
 )
+from discord_bot.alchemy import alchemy_recipes
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -68,21 +73,15 @@ async def on_ready():
     await validate_connection()
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+# @bot.event
+# async def on_message(message):
+#     if message.author == bot.user:
+#         return
 
-    if message.content.startswith("hello"):
-        await message.channel.send("Hello!")
+#     if message.content.startswith("hello"):
+#         await message.channel.send("Hello!")
 
-    await bot.process_commands(message)
-
-
-@bot.command(name="ping")
-async def ping(ctx):
-    await ctx.send("Pong!")
-
+#     await bot.process_commands(message)
 
 def find_prof(prof_str):
     available_professions = [prof.name for prof in Profession] + [
@@ -115,7 +114,7 @@ async def add_prof(ctx):
     for profession_str in profession_strs:
         profession = find_prof(profession_str)
         if profession is None:
-            await ctx.send(f'Profession "{profession_str}" not found.')
+            await ctx.send(f"Profession `{profession_str}` not found.")
             continue
 
         guild = ctx.guild
@@ -125,7 +124,7 @@ async def add_prof(ctx):
         await add_profession(author.id, profession.value)
 
         await ctx.send(
-            f'Registered profession "{profession.value}" for user "{author.display_name}".'
+            f"Registered profession `{profession.value}` for user `{author.display_name}`."
         )
 
 
@@ -150,7 +149,7 @@ async def remove_prof(ctx):
         await remove_profession(author.id, profession.value)
 
         await ctx.send(
-            f'Removed profession "{profession.value}" for user "{author.display_name}".'
+            f"Removed profession `{profession.value}` for user `{author.display_name}`."
         )
 
 
@@ -188,12 +187,201 @@ async def my_prof(ctx):
     user_professions = await get_user_professions(ctx.message.author.id)
     author = ctx.message.author
     if not user_professions:
-        await ctx.send(f'No professions registered for user "{author.display_name}".')
+        await ctx.send(f"No professions registered for user `{author.display_name}`.")
     else:
         await ctx.send(
-            f"Registered professions: {', '.join([profession["profession_name"] for profession in user_professions])}"
+            f"Registered professions: {', '.join([f'`{profession["profession_name"]}`' for profession in user_professions])}"
         )
 
 
-# Replace 'YOUR_BOT_TOKEN' with your bot's token
+@bot.command(name="add-recipe")
+async def add_recipe(ctx):
+    try:
+        user_professions = await get_user_professions(ctx.message.author.id)
+
+        if not user_professions:
+            await ctx.send(
+                "No professions registered. Add professions with `!add-prof`."
+            )
+            return
+
+        user_professions = [prof["profession_name"] for prof in user_professions]
+
+        search_recipes = [
+            recipe.strip()
+            for recipe in (" ".join(ctx.message.content.split(" ")[1:])).split(",")
+            if recipe.strip()
+        ]
+
+        if not search_recipes:
+            await ctx.send("Please provide one or more recipes to register.")
+            return
+
+        alchemy_recipe_names = [r.lower() for r in alchemy_recipes]
+
+        alchemy_matches = await _search_recipe(search_recipes, alchemy_recipe_names)
+
+        for key, recipes in alchemy_matches.items():
+            if not recipes:
+                await ctx.send(f"No recipe found for `{key}`.")
+                continue
+            else:
+                if len(recipes) > 1:
+                    await ctx.send(
+                        f"`{key}` matches multiples recipes: {', '.join([f'`{match}`' for match in recipes])}.\nPlease be more specific."
+                    )
+                    continue
+                else:
+                    matched_recipe = recipes[0]
+                    await add_user_recipe(ctx.message.author.id, matched_recipe)
+                    await ctx.send(f"Added recipe `{matched_recipe}`.")
+    except Exception as e:
+        logger.info(f"An error occurred: {e}")
+        await ctx.send("Failed to add recipes. (Something is broken)")
+
+
+@bot.command(name="remove-recipe")
+async def remove_recipe(ctx):
+    split = ctx.message.content.split(" ")
+    if len(split) < 2:
+        await ctx.send("Please provide a recipe to remove.")
+        return
+
+    recipe_strs = ctx.message.content.split(" ")[1:]
+    author = ctx.message.author
+    user_recipes = await get_user_recipes(author.id)
+    if not user_recipes:
+        await ctx.send(f"No recipes registered for user `{author.display_name}`.")
+        return
+
+    user_recipe_names = [recipe["recipe_name"] for recipe in user_recipes]
+    matches = await _search_recipe(recipe_strs, user_recipe_names)
+
+    if not matches:
+        await ctx.send(f"No recipe found for `{', '.join(recipe_strs)}`.")
+        return
+    else:
+        for recipe_str, recipes in matches.items():
+            for recipe in recipes:
+                await remove_user_recipe(author.id, recipe)
+                await ctx.send(
+                    f"Removed recipe `{recipe}` for user `{author.display_name}`."
+                )
+
+
+async def _search_recipe(
+    recipes: list[str], available_recipes: list[str]
+) -> dict[str, list[str]]:
+    result_groups = {recipe: [] for recipe in recipes}
+
+    for recipe in recipes:
+        matches = []
+
+        all_alchemy_recipes = [r.lower() for r in available_recipes]
+        if recipe in all_alchemy_recipes:
+            matches = [recipe]
+        else:
+            matches = difflib.get_close_matches(recipe.lower(), all_alchemy_recipes)
+            matches = [r for r in alchemy_recipes if recipe.lower() in r.lower()]
+
+        if not matches:
+            matches = [r for r in alchemy_recipes if recipe.lower() in r.lower()]
+
+        if matches:
+            better_matches = [
+                match for match in matches if recipe.lower() in match.lower()
+            ]
+            if better_matches:
+                matches = better_matches
+
+            if len(matches) == 1 or matches[0].lower() == recipe.lower():
+                matched_recipe = matches[0]
+                result_groups[recipe].append(matched_recipe)
+            else:
+                result_groups[recipe].extend(matches)
+
+    return result_groups
+
+
+@bot.command(name="search-recipe")
+async def search_recipe(ctx):
+    recipes = [
+        recipe.strip()
+        for recipe in (" ".join(ctx.message.content.split(" ")[1:])).split(",")
+    ]
+
+    all_recipes = list(alchemy_recipes.keys())
+
+    matches = await _search_recipe(recipes, all_recipes)
+
+    if matches:
+        await ctx.send(
+            f"Available recipes {', '.join([f'`{match}`' for match in matches])}."
+        )
+    else:
+        await ctx.send(f"No recipe found for `{', '.join(recipes)}`.")
+
+
+@bot.command(name="who")
+async def who(ctx):
+    search_recipes = [
+        recipe.strip()
+        for recipe in (" ".join(ctx.message.content.split(" ")[1:])).split(",")
+        if recipe.strip()
+    ]
+
+    if not search_recipes:
+        await ctx.send("Please provide one or more recipes to search.")
+        return
+
+    user_recipes = await get_all_recipes()
+    if not user_recipes:
+        await ctx.send(f"No recipe found for `{', '.join(search_recipes)}`.")
+        return
+
+    user_recipe_names = [recipe["recipe_name"] for recipe in user_recipes]
+
+    matches = await _search_recipe(search_recipes, user_recipe_names)
+
+    # flatten the matches
+    recipe_names = [match for match_list in matches.values() for match in match_list]
+
+    user_crafters = [
+        recipe for recipe in user_recipes if recipe["recipe_name"] in recipe_names
+    ]
+
+    recipe_user_craft_groups = {
+        recipe_name: [
+            recipe["user_name"]
+            for recipe in user_crafters
+            if recipe["recipe_name"] == recipe_name
+        ]
+        for recipe_name in {recipe["recipe_name"] for recipe in user_crafters}
+    }
+
+    if recipe_user_craft_groups:
+        for recipe_name in recipe_user_craft_groups:
+            await ctx.send(
+                f"Users that can craft `{recipe_name}`: {', '.join([f'`{user}`' for user in recipe_user_craft_groups[recipe_name]])}"
+            )
+    else:
+        await ctx.send(f"No recipe found for `{', '.join(search_recipes)}`.")
+
+
+@bot.command(name="commands")
+async def list_commands(ctx):
+    await ctx.send(
+        "Commands:\n"
+        "`!add-prof <profession>` - Add one or more professions\n"
+        "`!remove-prof <profession>` - Remove one or more professions\n"
+        "`!list-prof` - List all registered professions\n"
+        "`!my-prof` - List your registered professions\n"
+        "`!add-recipe <recipe>` - Add one or more recipes\n"
+        "`!remove-recipe <recipe>` - Remove one or more recipes\n"
+        "`!search-recipe <recipe>` - Search for a recipe\n"
+        # "!craft <recipe> - Display users that can craft an item\n"
+        "`!who <recipe>` - Display users that can craft an item\n"
+    )
+
+
 bot.run(DISCORD_BOT_TOKEN)
--- src/discord_bot/database.py
+++ src/discord_bot/database.py
@@ -24,7 +24,9 @@ async def validate_connection():
             for param, value in DATABASE_CONNECTION_PARAMS.items()
             if param != "password"
         }
-        logger.info(f"Connecting to the database using the following parameters: {params}")
+        logger.info(
+            f"Connecting to the database using the following parameters: {params}"
+        )
         # Establish the connection
         conn = await asyncpg.connect(**DATABASE_CONNECTION_PARAMS)
 
@@ -64,7 +66,9 @@ async def add_user(user_id: int, user_name: str):
             # Execute the insert query
             await conn.execute(upsert_query, *upsert_data)
 
-            logger.info(f"User {user_name} with ID {user_id} was added to the database.")
+            logger.info(
+                f"User {user_name} with ID {user_id} was added to the database."
+            )
         finally:
             # Close the connection
             await conn.close()
@@ -104,7 +108,9 @@ async def add_profession(user_id: int, profession: str):
 
 
 async def remove_profession(user_id: int, profession: str):
-    logger.info(f"Removing profession {profession} for user {user_id} from the database...")
+    logger.info(
+        f"Removing profession {profession} for user {user_id} from the database..."
+    )
 
     try:
         # Establish the connection
@@ -131,7 +137,7 @@ async def remove_profession(user_id: int, profession: str):
         logger.info(f"An error occurred: {e}")
 
 
-async def get_user_professions(user_id: int):
+async def get_user_professions(user_id: int) -> list[dict] | None:
     logger.info(f"Getting professions for user {user_id} from the database...")
 
     try:
@@ -148,7 +154,9 @@ async def get_user_professions(user_id: int):
             # Execute the insert query
             professions = await conn.fetch(query, *data)
 
-            logger.info(f"Professions for user {user_id} were retrieved from the database.")
+            logger.info(
+                f"Professions for user {user_id} were retrieved from the database."
+            )
             return professions
         finally:
             # Close the connection
@@ -158,7 +166,7 @@ async def get_user_professions(user_id: int):
         logger.info(f"An error occurred: {e}")
 
 
-async def get_all_professions() -> list[dict]|None:
+async def get_all_professions() -> list[dict] | None:
     logger.info(f"Getting all professions from the database...")
 
     try:
@@ -184,3 +192,113 @@ async def get_all_professions() -> list[dict]|None:
 
     except asyncpg.PostgresError as e:
         logger.info(f"An error occurred: {e}")
+
+
+async def add_user_recipe(user_id: int, recipe: str):
+    logger.info(f"Adding recipe {recipe} for user {user_id} to the database...")
+
+    # try:
+    # Establish the connection
+    conn = await asyncpg.connect(**DATABASE_CONNECTION_PARAMS)
+
+    try:
+        # Define the insert query and the data to be inserted
+        upsert_query = """
+            INSERT INTO recipes (user_id, recipe_name)
+            VALUES ($1, $2)
+            ON CONFLICT (user_id, recipe_name) DO NOTHING
+        """
+        upsert_data = (user_id, recipe)
+
+        # Execute the insert query
+        await conn.execute(upsert_query, *upsert_data)
+
+        logger.info(f"Recipe {recipe} for user {user_id} was added to the database.")
+    finally:
+        # Close the connection
+        await conn.close()
+
+
+async def get_user_recipes(user_id: int) -> list[dict] | None:
+    logger.info(f"Getting recipes for user {user_id} from the database...")
+
+    try:
+        # Establish the connection
+        conn = await asyncpg.connect(**DATABASE_CONNECTION_PARAMS)
+
+        try:
+            # Define the insert query and the data to be inserted
+            query = """
+                SELECT recipe_name FROM recipes WHERE user_id = $1
+            """
+            data = (user_id,)
+
+            # Execute the insert query
+            recipes = await conn.fetch(query, *data)
+
+            logger.info(f"Recipes for user {user_id} were retrieved from the database.")
+            return recipes
+        finally:
+            # Close the connection
+            await conn.close()
+
+    except asyncpg.PostgresError as e:
+        logger.info(f"An error occurred: {e}")
+
+
+async def remove_user_recipe(user_id: int, recipe: str):
+    logger.info(f"Removing recipe {recipe} for user {user_id} from the database...")
+
+    try:
+        # Establish the connection
+        conn = await asyncpg.connect(**DATABASE_CONNECTION_PARAMS)
+
+        try:
+            # Define the insert query and the data to be inserted
+            query = """
+                DELETE FROM recipes WHERE user_id = $1 AND recipe_name = $2
+            """
+            data = (user_id, recipe)
+
+            # Execute the insert query
+            await conn.execute(query, *data)
+
+            logger.info(
+                f"Recipe {recipe} for user {user_id} was removed from the database."
+            )
+        finally:
+            # Close the connection
+            await conn.close()
+
+    except asyncpg.PostgresError as e:
+        logger.info(f"An error occurred: {e}")
+
+
+async def get_all_recipes() -> list[dict] | None:
+    logger.info(f"Getting all recipes from the database...")
+    try:
+        # Establish the connection
+        conn = await asyncpg.connect(**DATABASE_CONNECTION_PARAMS)
+
+        try:
+            # Define the insert query and the data to be inserted
+            query = """
+                SELECT user_id, user_name, recipe_name
+                FROM recipes
+                JOIN users USING (user_id)
+            """
+
+            # Execute the insert query
+            recipes = await conn.fetch(query)
+
+            logger.info(f"All recipes were retrieved from the database.")
+            return recipes
+        finally:
+            # Close the connection
+            await conn.close()
+
+    except asyncpg.PostgresError as e:
+        logger.info(f"An error occurred: {e}")
+
+    # except asyncpg.PostgresError as e:
+    #     logger.info(f"An error occurred: {e}")
