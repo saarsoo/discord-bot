cc472e1024154a2434d4b9a8c936f772f945ae3d
Johan Saarsoo <johan@saarsoo.se>
2024-08-06 18:38:05 +0200

    Add profession enum module

---
+++ src/discord_bot/profession.py
@@ -0,0 +1,18 @@
+
+from enum import Enum
+
+
+class Profession(Enum):
+    alchemy = "Alchemy"
+    blacksmithing = "Blacksmithing"
+    enchanting = "Enchanting"
+    engineering = "Engineering"
+    leatherworking = "Leatherworking"
+    tailoring = "Tailoring"
+    herbalism = "Herbalism"
+    mining = "Mining"
+    skinning = "Skinning"
+    cooking = "Cooking"
+    fishing = "Fishing"
+    first_aid = "First aid"
+    lockpicking = "Lockpicking"
