389c315f161231623a31364011933701a1f32955
Johan Saarsoo <johan@saarsoo.se>
2024-08-07 18:09:30 +0200

    Add mining recipes

---
+++ src/discord_bot/mining.py
@@ -0,0 +1,14 @@
+mining_recipes = {
+    "Smelt Elementium": 22967,
+    "Smelt Thorium": 16153,
+    "Smelt Truesilver": 10098,
+    "Smelt Dark Iron": 14891,
+    "Smelt Mithril": 10097,
+    "Smelt Gold": 3308,
+    "Smelt Steel": 3569,
+    "Smelt Iron": 3307,
+    "Smelt Silver": 2658,
+    "Smelt Bronze": 2659,
+    "Smelt Tin": 3304,
+    "Smelt Copper": 2657,
+}
